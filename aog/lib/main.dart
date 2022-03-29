bae3016117510c137861b809cd9070548c92330b
ThiagoSolum <thiago.almeida@solum.agr.br>
2022-03-28 22:38:24 -0300

    criando widget de imagem para ela inicial

--- aog/lib/main.dart
+++ aog/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aog/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Qual compensa mais?',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const HomePage(),
     );
@@ -26,6 +27,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+        ],
+      ),
+    );
   }
 }
